b2245b1379486ca8a5b1f59d5c19a3f14ed3bffc
HaZeL-spc <kacper.leszczu8@gmail.com>
2021-09-16 16:32:11 +0200

    finalProject

--- App.js
+++ App.js
@@ -1,12 +1,79 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { View, StyleSheet, Animated, TouchableOpacity } from "react-native";
+import { CountdownCircleTimer } from "react-native-countdown-circle-timer";
+import { LinearGradient } from "expo-linear-gradient";
+import { Audio } from "expo-av";
 
 export default function App() {
+  const [hasStarted, setHasStarted] = useState(false);
+  const [recording, setRecording] = useState();
+  const [sound, setSound] = useState(null);
+  const [key, setKey] = useState(0);
+
+  useEffect(() => {
+    Audio.requestPermissionsAsync();
+  }, []);
+
+  const buttonHandler = async () => {
+    console.log(hasStarted);
+    setHasStarted(!hasStarted);
+
+    if (!hasStarted) {
+      try {
+        const recording = new Audio.Recording();
+        await recording.prepareToRecordAsync(
+          Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY
+        );
+        await recording.startAsync();
+        setRecording(recording);
+      } catch (err) {}
+    } else {
+      setKey(key + 1);
+      setRecording(undefined);
+      await recording.stopAndUnloadAsync();
+      const uri = recording.getURI();
+      console.log(uri);
+
+      const { sound } = await Audio.Sound.createAsync(
+        { uri: uri },
+        { shouldPlay: true }.uri
+      );
+      console.log(sound.playAsync());
+
+      setSound(sound);
+
+      await sound.playAsync();
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TouchableOpacity activeOpacity={0.5} onPress={buttonHandler}>
+        <CountdownCircleTimer
+          key={key}
+          isPlaying={hasStarted}
+          onComplete={buttonHandler}
+          duration={10}
+          colors={[
+            ["#34B3F6", 0.3],
+            ["#4145FB", 0.3],
+            ["#A741FB", 0.4],
+          ]}
+        >
+          {({ remainingTime, animatedColor }) => (
+            <LinearGradient
+              colors={["#34B3F6", "#4145FB", "#A741FB"]}
+              style={styles.linearGradient}
+            >
+              <Animated.Text
+                style={{ color: animatedColor, fontSize: 40, color: "white" }}
+              >
+                {remainingTime}
+              </Animated.Text>
+            </LinearGradient>
+          )}
+        </CountdownCircleTimer>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -14,8 +81,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#E9E9E9",
+    justifyContent: "space-around",
+    alignItems: "center",
+    flexDirection: "column",
+  },
+  linearGradient: {
+    width: 170,
+    height: 170,
+    borderRadius: 170 / 2,
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
